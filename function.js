3450d416ed88bc62cd40b2f10a6f6972effcb4a9
PatrickYang <azureroki@gmail.com>
2020-05-24 12:49:48 +0800

    'new_rule_to_play'

--- function.js
+++ function.js
@@ -1,137 +1,203 @@
-var BLOCK_SIZE = 20;
-var BLOCK_COUNT = 20;
-var snack;
-var apple;
-var score;
-var gameInterval;
+var canvas = document.getElementById("canvas_id");
 
-window.onload = function(){
-    document.addEventListener('keydown', handleKeyDown);
+var sizeOfBlock = 16;
+var numberOfBlock = 50;
+var snake = {};
+var apple = {};
+var score = "";
+var gameInterval = "";
+
+//game setting
+function gameStart() {
+  //
+  //start btn disappear when game start
+  document.getElementById("start").style.display = "none";
+
+  canvas.width = 800;
+  canvas.height = 800;
+
+  //game update rate and apple reput rate
+  gameInterval = setInterval(gameRoutine, 100);
+  appleInterval = setInterval(
+    putApple,
+    (Math.floor(Math.random() * 6) + 4) * 1000
+  );
+
+  snake = {
+    body: [{ x: sizeOfBlock, y: sizeOfBlock }],
+    size: 5,
+    direction: { x: 0, y: -1 },
+  };
+  putApple();
+  updateScore(0);
 }
-function handleKeyDown(event){
-var originX = snack.direction.x;
-var originY = snack.direction.y;
-    if(event.keyCode === 39){//left arrow
-        snack.direction.x = -originY;
-        snack.direction.y = originX;
-    }else if(event.keyCode === 37){//right arrow
-        snack.direction.x = originY;
-        snack.direction.y = -originX;
+
+//handling snake move rule
+function movesnake() {
+  var newBlock = {
+    x: snake.body[0].x + snake.direction.x,
+    y: snake.body[0].y + snake.direction.y,
+  };
+  snake.body.unshift(newBlock);
+  while (snake.body.length > snake.size) {
+    snake.body.pop();
+  }
+}
+
+//handling snake move direction
+window.onload = function () {
+  document.addEventListener("keydown", handleKeyDown);
+};
+function handleKeyDown(event) {
+  var originX = snake.direction.x;
+  var originY = snake.direction.y;
+  if (event.keyCode === 39) {
+    //left arrow
+    snake.direction.x = -originY;
+    snake.direction.y = originX;
+  } else if (event.keyCode === 37) {
+    //right arrow
+    snake.direction.x = originY;
+    snake.direction.y = -originX;
+  }
+}
+
+//handling score
+function updateScore(newScore) {
+  score = newScore;
+  document.getElementById("score_id").innerHTML = score;
+}
+
+//handling apple appear
+function putApple() {
+  //random appear
+  apple = {
+    x: Math.floor(Math.random() * sizeOfBlock),
+    y: Math.floor(Math.random() * sizeOfBlock),
+  };
+  console.log(apple);
+
+  //reput apple if apple appear on the snake
+  for (i = 0; i < snake.body.length; i++) {
+    if (apple.x === snake.body[i].x && apple.y === snake.body[i].y) {
+      putApple();
+      break;
     }
+  }
 }
 
-function gameStart(){
-    gameInterval = setInterval(gameRoutine, 100);
-    snack = {
-        body: [
-            { x: BLOCK_COUNT / 2, y: BLOCK_COUNT / 2 }
-        ],
-        size: 5,
-        direction: { x: 0, y:-1 }
+//game rule setting
+function gameRoutine() {
+  movesnake();
+  if (snakeIsDead()) {
+    ggler();
+    return;
+  }
+
+  //if hit the wall, reduce the wall then turn aroung and take a step
+  if (hitTheWall(snake.body[0])) {
+    reduceWall();
+    snake.body[0].x += hitTheWall(snake.body[0]).oneStepX;
+    snake.body[0].y += hitTheWall(snake.body[0]).oneStepY;
+    snake.direction.x = -snake.direction.x;
+    snake.direction.y = -snake.direction.y;
+  }
+  if (apple.x === snake.body[0].x && apple.y === snake.body[0].y) {
+    eatApple();
+  }
+  updateCanvas();
+}
+
+//game rule setting
+function eatApple() {
+  snake.size += 1;
+  apple = {};
+  updateScore(score + 1);
+}
+
+//game rule setting
+function hitTheWall(head) {
+  var step = {
+    oneStepX: 0,
+    oneStepY: 0,
+  };
+  //hit wall
+  if (head.x < 0) {
+    step.oneStepY = 1;
+    return step;
+  } else if (head.x >= numberOfBlock) {
+    head.x = numberOfBlock;
+    step.oneStepY = 1;
+    return step;
+  } else if (head.y < 0) {
+    step.oneStepX = 1;
+    return step;
+  } else if (head.y >= numberOfBlock) {
+    head.y = numberOfBlock;
+    step.oneStepX = 1;
+    return step;
+  }
+}
+
+//game rule setting
+function snakeIsDead() {
+  //
+  //hit body
+  for (i = 1; i < snake.body.length; i++) {
+    if (
+      snake.body[0].x === snake.body[i].x &&
+      snake.body[0].y === snake.body[i].y
+    ) {
+      return true;
     }
-    putApple();
-    updateScore(0);
+  }
+
+  //game over if canvas too small either
+  return canvas.width < 300 ? true : false;
+  return false;
 }
 
-function updateScore(newScore){
-    score = newScore;
-    document.getElementById('score_id').innerHTML = score;
+//game rule setting
+function reduceWall() {
+  canvas.width -= 113;
+  numberOfBlock = Math.floor(canvas.width / sizeOfBlock);
+  canvas.height -= 113;
+  numberOfBlock = Math.floor(canvas.height / sizeOfBlock);
 }
 
-function putApple(){
-    apple = {
-        x: Math.floor(Math.random() * BLOCK_COUNT),
-        y: Math.floor(Math.random() * BLOCK_COUNT)
-    }
-
-    for(i = 0; i < snack.body.length; i++){
-        if(apple.x === snack.body[i].x &&
-            apple.y === snack.body[i].y){
-                putApple();
-                break;
-            }
-    }
+//game over
+function ggler() {
+  clearInterval(appleInterval);
+  clearInterval(gameInterval);
+  document.getElementById("start").style.display = "block";
 }
 
-function gameRoutine(){
-    moveSnack();
-    if(snackIsDead()){
-        ggler();
-        return;
-    }
-    if(apple.x === snack.body[0].x &&
-        apple.y === snack.body[0].y){
-            eatApple();
-        }
-    updateCanvas();
-}
-
-function eatApple(){
-    snack.size += 1;
-    putApple();
-    updateScore(score + 1)
-}
-
-function snackIsDead(){
-    //hit wall
-    if(snack.body[0].x < 0){
-        return true;
-    }else if(snack.body[0].x >= BLOCK_COUNT){
-        return true;
-    }else if(snack.body[0].y < 0){
-        return true;
-    }else if(snack.body[0].y >= BLOCK_COUNT){
-        return true;
-    }
-
-    //hit body
-    for(i = 1; i < snack.body.length; i++){
-        if(snack.body[0].x === snack.body[i].x &&
-            snack.body[0].y === snack.body[i].y){
-                return true;
-            }
-    }
-    return false;
-}
-
-function ggler(){
-    clearInterval(gameInterval);
-}
-
-function moveSnack(){
-    var newBlock = {
-        x: snack.body[0].x + snack.direction.x,
-        y: snack.body[0].y + snack.direction.y
-    };
-
-    snack.body.unshift(newBlock);
-    while(snack.body.length > snack.size){
-        snack.body.pop();
-    };
-}
-
+//draw all stuff on canvas
 function updateCanvas() {
-    var canvas = document.getElementById('canvas_id');
-    if(canvas.getContext){
-        /** @type {CanvasRenderingContext2D} */
-        var context = canvas.getContext('2d');
-        context.fillStyle = 'black';
-        context.fillRect(0, 0, canvas.width, canvas.height);
-        context.fillStyle = 'lime';
-        for( i = 0; i < snack.body.length; i++ ){
-            context.fillRect(
-                snack.body[i].x * BLOCK_SIZE,
-                snack.body[i].y * BLOCK_SIZE,
-                BLOCK_SIZE -1,
-                BLOCK_SIZE -1
-            );
-        }
-        context.fillStyle = 'red';
-        context.fillRect(
-            apple.x * BLOCK_SIZE, 
-            apple.y * BLOCK_SIZE,
-            BLOCK_SIZE -1,
-            BLOCK_SIZE -1
-        )
+  if (canvas.getContext) {
+    //draw map
+    var context = canvas.getContext("2d");
+    context.fillStyle = "white";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    //draw snake
+    context.fillStyle = "lime";
+    for (i = 0; i < snake.body.length; i++) {
+      context.fillRect(
+        snake.body[i].x * sizeOfBlock,
+        snake.body[i].y * sizeOfBlock,
+        sizeOfBlock - 1,
+        sizeOfBlock - 1
+      );
     }
+
+    //draw apple
+    context.fillStyle = "red";
+    context.fillRect(
+      apple.x * sizeOfBlock,
+      apple.y * sizeOfBlock,
+      sizeOfBlock - 1,
+      sizeOfBlock - 1
+    );
+  }
 }
